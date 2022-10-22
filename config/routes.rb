fab0ffe724615a790a52734d8e40a8852eb41768
Roberto Barros <roberto.f.barros@gmail.com>
2022-10-22 11:47:14 -0300

    layout changed

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get 'orders', to: 'pages#order'
+  root to: "pages#home"
 end
